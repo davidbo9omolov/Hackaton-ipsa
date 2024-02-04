a8fe82248cb7dfd26c9253e34387c4fdf3f421c7
davidbogomolov <david.bogomolov2@gmail.com>
2024-02-04 08:51:24 -0500

    feat: linkable speakers

--- src/components/ui/speakers-block/SpeakersBlock.jsx
+++ src/components/ui/speakers-block/SpeakersBlock.jsx
@@ -5,7 +5,9 @@ const SpeakersBlock = ({props}) => {
     const elem = props.map((item) => {
       return(
             <div className={`${styles.speakersWrapp} ${styles[item.direction]}`}>
-                <img src={item.img} alt={item.name} className={styles.speakersImage}/>
+                <a href={item.link} className={styles.speakersImage}>
+                    <img src={item.img} alt={item.name} />
+                </a>
                 <div className={`${styles[item.flex]} ${styles.wrapper}`}>
                     <h1 className={styles.speakerName}>{item.name}</h1>
                     <h2 className={`${styles.speakerName} mb-10`}>{item.speciality}</h2>
